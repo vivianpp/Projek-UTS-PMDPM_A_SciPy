ca3e221eb0ff750156f1597cf171b2014d025c7c
vivianpp <220711795@students.uajy.ac.id>
2024-10-25 23:46:41 +0700

    Update MainStreamlit_A_SciPy.py

--- MainStreamlit_A_SciPy.py
+++ MainStreamlit_A_SciPy.py
@@ -52,40 +52,40 @@ if selected == 'Analisis Kategori Properti':
         prediction = rf_model.predict(input_data)[0]
         kategori_map = {0: 'Basic', 1: 'Luxury', 2: 'Middle'}
         kategori_properti = kategori_map.get(prediction, "Tidak Diketahui")
-        st.success(f"Kategori properti: **{kategori_properti}**")
+        st.success(f"Kategori properti: *{kategori_properti}*")
 
-if selected == 'Regresi':
-    st.title('Regresi')
+if selected == 'Analisis Harga Properti':
+    st.title('Regresi Harga Properti')
 
-    st.write('Untuk Inputan File dataset (csv) bisa menggunakan st.file_uploader')
-    file = st.file_uploader("Masukkan File", type=["csv", "txt"])
-    st.write('Untuk usia bisa menggunakan st.slider')
-    Age = st.slider("Age", 0, 100)
-    st.write('Untuk jenis kelamin bisa menggunakan st.radio')
-    Sex = st.radio("Gender", ["Female", "Male"])
-    st.write('Untuk beberapa kolom bisa menggunakan st.selectbox')
-    nama_kolom = st.selectbox("Nama Kolom", ["Under", "Normal", "Over"])
-    
-    # Input untuk panjang dan lebar
-    st.write('Untuk inputan manual bisa menggunakan st.number_input')
-    panjang = st.number_input("Masukan Input", 0)
-    lebar = st.number_input("Masukan Nilai Lebar", 0)
-    
-    alas = st.slider("Masukkan Nilai Alas", 0, 100)
-    tinggi = st.slider("Masukkan Nilai Tinggi", 0, 100)
-    st.write('Tombol button (Menggunakan st.button)')
-    hitung = st.button("Prediksi")
+    squaremeters = st.slider("Squaremeters", 0, 100000)
+    numberofrooms = st.slider("Number of Rooms", 0, 100)
+    hasyard = st.radio("Has Yard?", ["Yes", "No"])
+    haspool = st.radio("Has Pool?", ["Yes", "No"])
+    floors = st.number_input("Floors", 0)
+    citycode = st.number_input("City Code", 0)
+    citypartrange = st.number_input("City Part Range", 0)
+    numprevowners = st.number_input("Number of Previous Owners", 0)
+    made = st.number_input("Year Built", 0)
+    isnewbuilt = st.radio("Is New Built?", ["New", "Old"])
+    hasstormprotector = st.radio("Has Storm Protector?", ["Yes", "No"])
+    basement = st.number_input("Basement Area", 0)
+    attic = st.number_input("Attic Area", 0)
+    garage = st.number_input("Garage Area", 0)
+    hasstorageroom = st.radio("Has Storage Room?", ["Yes", "No"])
+    hasguestroom = st.number_input("Number of Guest Rooms", 0)
 
-    if hitung:
-        luas = 0.5 * alas * tinggi
-        st.write("Luas Segitiga Adalah", luas)
+    data_input = pd.DataFrame([[squaremeters, numberofrooms, hasyard == "Yes", haspool == "Yes", floors,
+                                citycode, citypartrange, numprevowners, made, isnewbuilt == "New",
+                                hasstormprotector == "Yes", basement, attic, garage, 
+                                hasstorageroom == "Yes", hasguestroom]],
+                              columns=['squaremeters', 'numberofrooms', 'hasyard', 'haspool', 'floors',
+                                       'citycode', 'citypartrange', 'numprevowners', 'made', 
+                                       'isnewbuilt', 'hasstormprotector', 'basement', 'attic', 
+                                       'garage', 'hasstorageroom', 'hasguestroom'])
 
-# Halaman ketentuan
-if selected == 'Catatan':
-    st.title('Catatan')
-    st.write('''1. Untuk memunculkan sidebar agar tidak error ketika di run, silahkan install library streamlit option menu di terminal dengan perintah "pip install streamlit-option-menu".')
-    st.write('2. Menu yang dibuat ada 2 yaitu Klasifikasi dan Regresi.''')
-    st.write('3. Inputnya apa saja, sesuaikan dengan arsitektur code anda pada notebook.')
-    st.write('4. Referensi desain streamlit dapat di akses pada https://streamlit.io/')
-    st.write('5. Link streamlit design ini dapat di akses pada https://apputs-6qzfrvr4ufiyzhj84mrfkt7.streamlit.app/')
-    st.write('''6. Library dan file requirements yang dibutuhkan untuk deploy online di github ada 5 yaitu streamlit, scikit-learn, pandas, numpy, streamlit-option-menu.''')
+    if st.button("Prediksi Harga"):
+        try:
+            harga = lr_model.predict(data_input)[0]
+            st.success(f"Harga Properti: Rp {harga:,.0f}")
+        except ValueError as e:
+            st.error(f"Error dalam prediksi: {e}")
